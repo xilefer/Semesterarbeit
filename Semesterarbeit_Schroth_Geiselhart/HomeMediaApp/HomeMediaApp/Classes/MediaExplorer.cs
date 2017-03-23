2c85242ea3175e4bdfa85715da44a0543fbecb04
Felix Geiselhart <i14009@hb.dhbw-stuttgart.de>
2017-03-23 14:59:25 +0100

    Dateien für Fileexplorer eingefügt

---
+++ Semesterarbeit_Schroth_Geiselhart/HomeMediaApp/HomeMediaApp/Classes/MediaExplorer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HomeMediaApp.Classes
+{
+    public enum FileExplorerItemType
+    {
+        FOLDER,
+        PICTURE,
+        VIDEO,
+        MUSIC,
+        ELSE
+    };
+
+    public class FileExplorerItemBase
+    {
+        public ImageSource IconSource { get; set; } = new FileImageSource();
+        public string DisplayName { get; set; } = "";
+        public FileExplorerItemType ItemType { get; set; } = FileExplorerItemType.ELSE;
+    }
+
+    public class FolderItem : FileExplorerItemBase
+    {
+        public FolderItem(string FolderName)
+        {
+            base.ItemType = FileExplorerItemType.FOLDER;
+            base.DisplayName = FolderName;
+            base.IconSource = ImageSource.FromResource("HomeMediaApp.Icons.folder_icon.png");
+        }
+    }
+}
